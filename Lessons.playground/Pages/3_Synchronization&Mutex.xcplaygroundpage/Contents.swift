f8675c998dc96ef8d639f04353fb8b6f8d44a39c
Brado Brain <vinogradov.artem@outlook.com>
2022-07-14 13:15:00 +0300

    Understanding Synchronization & Mutex in C and Obj-C

---
+++ Lessons.playground/Pages/3_Synchronization&Mutex.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Synchronization & Mutex
+
+
+// C mutex function
+class SaveThread {
+    private var mutex = pthread_mutex_t()
+    
+    init() {
+        pthread_mutex_init(&mutex, nil)
+    }
+    
+    func someMethod(completion: () -> ()) {
+        pthread_mutex_lock(&mutex) // locking access
+        completion() // do somthing what we want
+        do { // to garantee that our thread will be unlocked
+            pthread_mutex_unlock(&mutex) // unlocking access
+        }
+    }
+}
+
+var array: [String] = []
+let saveThread = SaveThread()
+
+saveThread.someMethod {
+    print("Test mutex in C")
+    array.append("First Thread")
+}
+
+array.append("Second Thread")
+print(array)
+
+// Obj-C mutex function
+class SaveThreadObjC {
+    private let lockMutex = NSLock()
+    
+    func anotherMethod(completion: () -> ()) {
+        lockMutex.lock()
+        completion()
+        do {
+            lockMutex.unlock()
+        }
+    }
+}
+
+var objCArray: [String] = []
+let saveThreadObjC = SaveThreadObjC()
+
+saveThreadObjC.anotherMethod {
+    print("Test mutex in Obj-C")
+    objCArray.append("First Data")
+}
+
+objCArray.append("Second Data")
+print(objCArray)
+//: [Next](@next)
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Topic of learning in this repository is Multithreading
 
 Lesson 1: Thread in C and Obj-C
+
 Lesson 2: Quality Of Service in C and Obj-C
